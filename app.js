0a39a31bd11fc4faaf67681729b2d47539ee4272
Vinicius Uehara <viuehara12@gmail.com>
2021-07-15 16:05:58 -0300

    chore: change port in order to deploy on heroku

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const bogosortController = require("./api/controllers/bogosortController");
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
+    6;
     res.sendFile("public/index.html", { root: path.dirname(__dirname) });
 });
 
